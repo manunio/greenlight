74fee4059766996e1ddd786de22933158e2c0a5e
manunio <max.manu.nair@gmail.com>
2022-04-13 17:58:00 +0530

    fix ssl error for pq package by disabling sslmode=disable query parameter in dsn.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -36,7 +36,8 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "development|staging|production")
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgresql://localhost:5431/greenlight", "PostgreSQL DSN")
+	// TODO: / NOTE: don't hard-code dsn, and pass it via flag
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost:5431/greenlight?sslmode=disable", "PostgreSQL DSN")
 
 	flag.Parse()
 
