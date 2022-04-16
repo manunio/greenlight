57d9a3fed7c5c322f881dbde399bc011d810a512
manunio <max.manu.nair@gmail.com>
2022-04-16 19:32:35 +0530

    added token based global rate limiter

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -20,5 +20,5 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMovieHandler)
 
-	return app.recoverPanic(router)
+	return app.recoverPanic(app.rateLimit(router))
 }
