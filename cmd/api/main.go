a4c4611a132e6a65ab4f1d15127c87cfb0dea828
manunio <max.manu.nair@gmail.com>
2022-04-16 21:57:59 +0530

    add configuration for rate limiter via flags and added ip based rate limiter

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -29,6 +29,14 @@ type config struct {
 		maxIdleConns int
 		maxIdleTime  string
 	}
+	// limiter struct contains fields for the requests-per-second and burst
+	// values, and a boolean field which we can use to enable/disable rate limiting
+	// altogether.
+	limiter struct {
+		rps     float64
+		burst   int
+		enabled bool
+	}
 }
 
 type application struct {
@@ -48,6 +56,11 @@ func main() {
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
+	// rate limiter related flags
+	flag.Float64Var(&cfg.limiter.rps, "limiter-rps", 2, "Rate limiter maximum requests per second")
+	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
+	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
 	flag.Parse()
 
